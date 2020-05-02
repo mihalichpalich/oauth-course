6b486573c9f519392aa1b7c8e56cbbf1b422be55
mihalichpalich <pavlovtfspb@gmail.com>
2020-05-03 02:09:53 +0300

    все сделал

---
+++ config/keys-example.js
@@ -0,0 +1,14 @@
+//https://console.developers.google.com/apis/credentials/wizard?organizationId=0&project=aqueous-sandbox-275910
+//https://cloud.mongodb.com/v2/5eac4aff2a3d195b7d108230#clusters
+module.exports = {
+    google: {
+        clientID: '',
+        clientSecret: ''
+    },
+    mongodb: {
+        dbURI: ''
+    },
+    session: {
+        cookieKey: '' //25 symbols
+    }
+};
--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
-const app = express;
+const authRoutes = require('./routes/auth-routes');
+const profileRoutes = require('./routes/profile-routes');
+const passportSetup = require('./config/passport-setup');
+const keys = require('./config/keys');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [keys.session.cookieKey]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect(keys.mongodb.dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+app.get('/', (req, res) => {
+    res.render("Home");
+});
+
+app.listen(3000, () => {
+    console.log("server run");
+});
